309ae63cbb44ba038bba492132497eb71b562395
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2025-04-05 13:52:59 +0900

    facade environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrivateKey } from 'symbol-sdk';
+import { SymbolFacade, descriptors, models } from 'symbol-sdk/symbol';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +17,8 @@ export class AppComponent {
       // グローバルなTextDecoderとTextEncoderを使用
       console.log('TextDecoder:', window.TextDecoder);
       console.log('TextEncoder:', window.TextEncoder);
+      const facade = new SymbolFacade('testnet');
+      console.log(facade)
 
       // 動作確認
       alert('設定の修正が完了しました');
